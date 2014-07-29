88ae0d95cab8ddce867487d04701b3375cabe767
Gilles Crettenand <gilles.crettenand@liip.ch>
2014-07-29 08:30:04 +0200

    Click to show password

--- popin.js
+++ popin.js
@@ -3,28 +3,38 @@ var Popin = function() {
 
     var container = $('.credentials');
     var credential_class = 'credential';
+    var password_stars_class = 'password-stars';
+    var password_real_class = 'password-real';
 
 
     function display_credentials(credentials) {
-        container.find('.');
-        $('.' + credential_class).remove();
+        container.find('.' + credential_class).remove();
 
         for (var key in credentials) {
             if (credentials.hasOwnProperty(key)) {
                 var c = credentials[key];
                 var elem = $(
-                        '<tr class="' + credential_class + '">' +
+                    '<tr class="' + credential_class + '">' +
                         '<th class="index">' + key + '</th>' +
                         '<td class="url">' + c.url + '</td>' +
                         '<td class="username">' + c.username + '</td>' +
-                        '<td class="password">***</td>' +
+                        '<td class="password">' +
+                            '<span class="' + password_stars_class + '">***</span>' +
+                            '<span class="' + password_real_class + '">' + c.password + '</span>' +
+                        '</td>' +
                         '<td class="action"><button class="remove" data-id="' + key + '">Remove</button></td>' +
-                        '</tr>'
+                    '</tr>'
                 );
 
                 container.prepend(elem);
             }
         }
+    }
+
+    function togglePassword() {
+        var password = $(this).parent();
+        password.find('.' + password_stars_class).toggle();
+        password.find('.' + password_real_class).toggle();
     }
 
     function add() {
@@ -51,6 +61,7 @@ var Popin = function() {
 
         $('#add').on('click', add);
         $(document).on('click', '.remove', remove);
+        $(document).on('click', '.password > span', togglePassword);
     }
 
     return {
