18cc2f9d177235821e60a7c98c8993f906097584
Gilles Crettenand <gilles.crettenand@liip.ch>
2014-08-04 23:30:28 +0200

    remove LICENSE from package

--- package.sh
+++ package.sh
@@ -3,7 +3,7 @@
 DIR=$(pwd)
 
 cd ..
-zip MultiPass.zip -r $DIR -x *.git* -x *.editorconfig* -x *package.sh* -x *.idea* -x *README.md* -x *icon-original.png*
+zip MultiPass.zip -r $DIR -x *.git* -x *.editorconfig* -x *package.sh* -x *.idea* -x *README.md* -x *LICENSE* -x *icon-original.png*
 mv MultiPass.zip $DIR
 
 
