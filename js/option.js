8785b7f412892ae315a4e285cd028b59a82c5327
Gilles Crettenand <gilles.crettenand@liip.ch>
2014-08-04 23:11:34 +0200

    Basic file import

--- js/option.js
+++ js/option.js
@@ -1,7 +1,60 @@
 var Option = function() {
     'use strict';
 
+    var credentials = [];
+    var import_output;
+
     function init() {
+        import_output = $('output.import-list');
+
+        $('#import').on('change', function(e) {
+            var files = e.target.files;
+
+            var ul = $('<ul>');
+            import_output.empty().append(ul);
+
+            credentials = [];
+            for (var i = 0, file; file = files[i]; ++i) {
+                var reader = new FileReader();
+                reader.onloadend = function (event) {
+                    if (event.target.readyState == FileReader.DONE) {
+                        try {
+                            var json = JSON.parse(event.target.result);
+
+                            for (var key in json) {
+                                if (json.hasOwnProperty(key)) {
+                                    var c = json[key];
+                                    credentials.push(c);
+
+                                    var li = $('<li>' + c.url + ' : ' + c.username + ' - ' + c.password + '</li>');
+                                    ul.append(li);
+                                }
+                            }
+                        } catch(err) {
+                            console.log('JSON file is malformed.');
+                            console.log(event);
+                        }
+                    } else {
+                        console.log('Error reading the file.');
+                        console.log(event);
+                    }
+                };
+                reader.readAsText(file);
+            }
+        });
+
+        $('button.import-submit').on('click', function(event) {
+            for (var key in credentials) {
+                if (credentials.hasOwnProperty(key)) {
+                    Storage.addCredential(credentials[key]);
+                }
+            }
+
+            credentials = [];
+            import_output.empty();
+
+            event.preventDefault();
+        });
     }
 
     return {
