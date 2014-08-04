699b666b7c383e5b5ca506059bd336aad8de9505
Gilles Crettenand <gilles.crettenand@liip.ch>
2014-08-04 19:01:58 +0200

    better packaging

--- package.sh
+++ package.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-zip MultiPass.zip -r MultiPass -x *.git* -x *.editorconfig* -x *package.sh* -x *.idea* -x *README.md* -x *icon-original.png*
+DIR=$(pwd)
+
+cd ..
+zip MultiPass.zip -r $DIR -x *.git* -x *.editorconfig* -x *package.sh* -x *.idea* -x *README.md* -x *icon-original.png*
+mv MultiPass.zip $DIR
+
+
