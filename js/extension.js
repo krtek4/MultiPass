71aaf874fc3ba7e247cce8db53458b4b163ad532
Gilles Crettenand <gilles.crettenand@liip.ch>
2014-08-05 17:42:31 +0200

    Avoir error loop by trying only 5 times in a row. fixes #9

--- js/extension.js
+++ js/extension.js
@@ -1,16 +1,32 @@
 var Extension = function () {
     'use strict';
 
+
+    var last_request_id = '';
+    var last_tab_id = '';
+    var try_count = 0;
+
     function retrieveCredentials(status) {
         var credential = Storage.getForUrl(status.url);
 
         if (credential.hasOwnProperty('username') && credential.hasOwnProperty('password')) {
-            return {
-                authCredentials: {
-                    username: credential.username,
-                    password: credential.password
-                }
-            };
+            if (status.requestId == last_request_id && status.tabId == last_tab_id) {
+                ++try_count;
+            } else {
+                try_count = 0;
+            }
+
+            if(try_count < 5) {
+                last_request_id = status.requestId;
+                last_tab_id = status.tabId;
+
+                return {
+                    authCredentials: {
+                        username: credential.username,
+                        password: credential.password
+                    }
+                };
+            }
         }
 
         return {};
