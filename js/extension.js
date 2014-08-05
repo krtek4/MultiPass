de686cac8ed23dc544bd0cf1a8ec62c1280043b8
Gilles Crettenand <gilles.crettenand@liip.ch>
2014-08-05 17:55:39 +0200

    Skeleton for notification system. #10 #11

--- js/extension.js
+++ js/extension.js
@@ -5,6 +5,20 @@ var Extension = function () {
     var last_request_id = '';
     var last_tab_id = '';
     var try_count = 0;
+    var success_timeout;
+
+    function showNotification(title, message, timeout) {
+        timeout = (typeof timeout !== 'undefined') ? timeout : 0;
+
+        console.log(title + ' : ' + message);
+
+        // Hide notification
+        if (timeout > 0) {
+            setTimeout(function() {
+                console.log('notification timeout reached')
+            }, timeout);
+        }
+    }
 
     function retrieveCredentials(status) {
         var credential = Storage.getForUrl(status.url);
@@ -20,12 +34,23 @@ var Extension = function () {
                 last_request_id = status.requestId;
                 last_tab_id = status.tabId;
 
+                if(typeof(success_timeout) === 'undefined' || success_timeout === '') {
+                    success_timeout = setTimeout(function () {
+                        success_timeout = '';
+                        showNotification("Success", "MultiPass logged you in with success.", 200);
+                    }, 1000);
+                }
+
                 return {
                     authCredentials: {
                         username: credential.username,
                         password: credential.password
                     }
                 };
+            } else {
+                clearTimeout(success_timeout);
+                success_timeout = '';
+                showNotification("Error", "The credentials provided were not accepted", 500);
             }
         }
 
