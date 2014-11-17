82f742a072484834bdf4a3c298666dfb254fb626
Gilles Crettenand <gilles.crettenand@liip.ch>
2014-11-17 10:49:39 +0100

    Highlight regex matching the current tab

--- js/popin.js
+++ js/popin.js
@@ -1,13 +1,44 @@
 var Popin = function() {
     'use strict';
 
+    var only_match = 'only-match';
+    var multiple_match = 'multiple-match';
+
     function optionLink() {
         Analytics.event('Popin', 'option link');
         chrome.tabs.create({'url': chrome.extension.getURL("options.html") })
     }
 
+    function highlightUrlForTab(tab) {
+        var container = $('.credentials');
+        container.find('tr').removeClass(only_match + ' ' + multiple_match);
+
+        var matches = [];
+        container.find('.url').each(function() {
+            var re = new RegExp($(this).text());
+            if (re.test(tab.url)) {
+                matches.push($(this).parent());
+            }
+        });
+
+        var clazz = matches.length > 1 ? multiple_match : only_match;
+        matches.map(function(a) { a.addClass(clazz); });
+    }
+
+    function highlightUrlForTabId(tab_id) {
+        chrome.tabs.get(tab_id, highlightUrlForTab);
+    }
+
+    function highlightUrlForStatus(status) {
+        highlightUrlForTabId(status.tabId);
+    }
+
     function init() {
         $('.option-link').on('click', optionLink);
+
+        chrome.tabs.getSelected(null, highlightUrlForTab);
+        chrome.tabs.onUpdated.addListener(highlightUrlForTabId);
+        chrome.tabs.onActivated.addListener(highlightUrlForStatus);
     }
 
     return {
