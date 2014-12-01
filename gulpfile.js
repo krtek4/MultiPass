03fdd5481d2d6ccebaa5d2f4040f167f2ba4dcfa
Gilles Crettenand <gilles.crettenand@liip.ch>
2014-12-01 17:46:50 +0100

    use gulp to build extension, prepare way for multi-browser

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+var gulp     = require('gulp');
+var zip      = require('gulp-zip');
+var clean    = require('gulp-clean');
+var rseq     = require('run-sequence');
+var es       = require('event-stream');
+
+var chrome   = require('./vendor/chrome/manifest');
+
+function pipe(src, transforms, dest) {
+  if (typeof transforms === 'string') {
+    dest = transforms;
+    transforms = null;
+  }
+
+  var stream = gulp.src(src);
+  transforms && transforms.forEach(function(transform) {
+    stream = stream.pipe(transform);
+  });
+
+  if (dest) {
+    stream = stream.pipe(gulp.dest(dest));
+  }
+
+  return stream;
+}
+
+gulp.task('clean', function() {
+  return pipe('./build', [clean()]);
+});
+
+gulp.task('chrome', function() {
+  return es.merge(
+    pipe('./*.html', './build/chrome/'),
+    pipe('./images/**/*', './build/chrome/images'),
+    pipe('./js/**/*', './build/chrome/js'),
+    pipe('./css/**/*', './build/chrome/css'),
+    pipe('./_locales/**/*', './build/chrome/_locales'),
+    pipe('./vendor/chrome/extension.js', './build/chrome/js'),
+    pipe('./vendor/chrome/popin.js', './build/chrome/js'),
+    pipe('./vendor/chrome/manifest.json', './build/chrome/')
+  );
+});
+
+gulp.task('chrome-dist', function () {
+  gulp.src('./build/chrome/**/*')
+    .pipe(zip('chrome-extension-' + chrome.version + '.zip'))
+    .pipe(gulp.dest('./dist/chrome'));
+});
+
+gulp.task('default', function(cb) {
+    return rseq('clean', ['chrome'], cb);
+});
+
+gulp.task('watch', function() {
+  gulp.watch(['./js/**/*', './css/**/*', './vendor/**/*', './images/**/*'], ['default']);
+});
--- package.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DIR=$(pwd)
-
-cd ..
-zip MultiPass.zip -r $DIR -x *.git* -x *.editorconfig* -x *package.sh* -x *.idea* -x *README.md* -x *LICENSE* -x *icon-original.png* -x *CHANGELOG.md* -x *MultiPass.zip*
-mv MultiPass.zip $DIR
-
-
