d797d7ab22a9503b93d38cb5edfc9a09ecff2dd0
Gilles Crettenand <gilles.crettenand@liip.ch>
2015-09-24 01:09:01 +0200

    move more things to React

--- js/components/table.js
+++ js/components/table.js
@@ -3,6 +3,8 @@
 var React = require('react');
 var ui = require('material-ui');
 
+var Analytics = require('../analytics');
+var CredentialStorage = require('../credential_storage');
 var Translator = require('../translator');
 
 var PasswordCell = React.createClass({
@@ -26,15 +28,21 @@ var PasswordCell = React.createClass({
 });
 
 var CredentialRow = React.createClass({
+    clickRemove: function() {
+        this.props.remove(this.props.url);
+    },
+    clickEdit: function() {
+        this.props.edit(this.props.url, this.props.username, this.props.password);
+    },
     render: function() {
         return (
-            <tr>
+            <tr className={this.props.editing ? 'editing' : ''}>
                 <td className="url">{this.props.url}</td>
                 <td className="username">{this.props.username}</td>
                 <PasswordCell password={this.props.password} />
                 <td>
-                    <button type="button" className="remove" data-url={this.props.url}>{Translator.translate("remove_credential")}</button>
-                    <button type="button" className="edit" data-url={this.props.url}>{Translator.translate("edit_credential")}</button>
+                    <button type="button" onClick={this.clickRemove}>{Translator.translate("remove_credential")}</button>
+                    <button type="button" onClick={this.clickEdit}>{Translator.translate("edit_credential")}</button>
                 </td>
             </tr>
         );
@@ -48,28 +56,97 @@ var CredentialTable = React.createClass({
     childContextTypes: {
         muiTheme: React.PropTypes.object,
     },
-
     getChildContext: function() {
         return {
             muiTheme: ThemeManager.getCurrentTheme(),
         };
     },
-
     componentWillMount: function() {
         ThemeManager.setPalette({
             accent1Color: Colors.deepOrange500
         });
     },
 
-   render: function() {
-       var rows = [];
-       for(var i in this.props.credentials) {
-           if(this.props.credentials.hasOwnProperty(i)) {
-               var c = this.props.credentials[i];
-               rows.push(<CredentialRow key={c.url} url={c.url} username={c.username} password={c.password}/>)
-           }
-       }
-       return (
+    getInitialState: function() {
+        return {
+            url: '',
+            username: '',
+            password: '',
+            old_url: false
+        };
+    },
+    getState: function() {
+        return this.state;
+    },
+
+    editCredential: function(url, username, password) {
+        this.setState({
+            url: url,
+            username: username,
+            password: password,
+            old_url: url
+        });
+    },
+    addCredential: function() {
+        var values = {
+            url: this.state.url,
+            username: this.state.username,
+            password: this.state.password
+        };
+
+        var valid = true;
+        for (var key in values) {
+            if (values.hasOwnProperty(key)) {
+                var v = values[key];
+
+                if(v === '') {
+                    console.error('Error - ' + key + ' : ' + v + ' is empty !');
+                    valid = false;
+                }
+            }
+        }
+
+        if(valid) {
+            if(this.state.old_url.length > 0) {
+                CredentialStorage.removeCredential(this.state.old_url);
+                Analytics.event('Credentials', 'edited');
+            } else {
+                Analytics.event('Credentials', 'added');
+            }
+
+            CredentialStorage.addCredential(values);
+            this.reset();
+        } else {
+            Analytics.event('Credentials', 'error in form');
+        }
+    },
+    removeCredential: function(url) {
+        CredentialStorage.removeCredential(url);
+        Analytics.event('Credentials', 'removed');
+    },
+    handleChange: function(event) {
+        var state = {};
+        state[event.target.id] = event.target.value;
+        this.setState(state);
+    },
+    reset: function() {
+        this.setState(this.getInitialState());
+    },
+
+    render: function() {
+        var rows = [];
+        for (var i in this.props.credentials) {
+            if (this.props.credentials.hasOwnProperty(i)) {
+                var c = this.props.credentials[i];
+                rows.push(<CredentialRow key={c.url}
+                                         url={c.url} username={c.username} password={c.password}
+                                         editing={c.url == this.state.old_url}
+                                         remove={this.removeCredential} edit={this.editCredential}/>)
+            }
+        }
+
+        var add_label = this.state.editing ? 'edit_credential' : 'add_credential';
+        return (
            <form id="credential-form">
                <table className="credential-table">
                    <thead>
@@ -86,17 +163,17 @@ var CredentialTable = React.createClass({
                    <tfoot>
                        <tr>
                            <td>
-                               <input id="url" name="url" hintText="URL, this is a regexp" />
+                               <input value={this.state.url} onChange={this.handleChange} id="url" name="url" placeholde="URL, this is a regexp" />
                            </td>
                            <td>
-                               <input id="username" name="username" hintText="Username" />
+                               <input value={this.state.username} onChange={this.handleChange} id="username" name="username" placeholde="Username" />
                            </td>
                            <td>
-                               <input id="password" name="password" hintText="Password" />
+                               <input value={this.state.password} onChange={this.handleChange} id="password" name="password" placeholde="Password" />
                            </td>
                            <td>
-                               <button className="credential-form-submit" type="submit">{Translator.translate("add_credential")}</button>
-                               <button className="credential-form-reset" type="reset">{Translator.translate("reset_credential")}</button>
+                               <button className="credential-form-submit" type="submit" onClick={this.addCredential}>{Translator.translate(add_label)}</button>
+                               <button className="credential-form-reset" type="reset" onClick={this.reset}>{Translator.translate("reset_credential")}</button>
                            </td>
                        </tr>
                    </tfoot>
--- js/credentials.js
+++ js/credentials.js
@@ -1,127 +1,30 @@
 'use strict';
 
-var $ = require('jquery');
+var React = require('react');
 
-var Analytics = require('./analytics');
 var CredentialStorage = require('./credential_storage');
 var Storage = require('./storage');
-var Translator = require('./translator');
 
-var React = require('react');
 var Table = require('./components/table');
 
 module.exports = function() {
     var storage_key = 'temporary-credentials';
+    var table;
 
     function display_credentials(credentials) {
-        React.render(<Table credentials={credentials} />, $('.credential-table-container')[0]);
-    }
-
-    function submit(e) {
-        e.preventDefault();
-
-        var url = $('#url');
-        var username = $('#username');
-        var password = $('#password');
-
-        var values = {
-            url: url.val(),
-            username: username.val(),
-            password: password.val()
-        };
-
-        var valid = true;
-        for (var key in values) {
-            if (values.hasOwnProperty(key)) {
-                var v = values[key];
-
-                if(v === '') {
-                    console.error('Error - ' + key + ' : ' + v + ' is empty !');
-                    valid = false;
-                }
-            }
-        }
-
-        if(valid) {
-            var old = $('tr.editing .url');
-            if(old.length > 0 && old.text().length > 0) {
-                CredentialStorage.removeCredential(old.text());
-            }
-
-            CredentialStorage.addCredential(values);
-
-            url.val('');
-            username.val('');
-            password.val('');
-
-            reset_form();
-
-            Analytics.event('Credentials', 'added');
-        } else {
-            Analytics.event('Credentials', 'error in form');
-
-        }
-    }
-
-    function remove(e) {
-        e.preventDefault();
-
-        var url = $(e.currentTarget).data('url');
-        CredentialStorage.removeCredential(url);
-
-        Analytics.event('Credentials', 'removed');
-    }
-
-    function edit(e) {
-        e.preventDefault();
-
-        var url = $('#url');
-        var username = $('#username');
-        var password = $('#password');
-
-        var tr = $(e.currentTarget).parents('tr');
-
-        tr.addClass('editing');
-
-        url.val(tr.find('.url').text());
-        username.val(tr.find('.username').text());
-        password.val(tr.find('.' + password_real_class).text());
-
-        $('.credential-form-submit').text(Translator.translate('edit_credential'));
-    }
-
-    function reset_form() {
-        $('tr.editing').removeClass('editing');
-        $('.credential-form-submit').text(Translator.translate('add_credential'));
+        table = React.render(<Table credentials={credentials} />, document.getElementById('credential-table-container'));
     }
 
     function init() {
         CredentialStorage.register(display_credentials);
 
-        $(document).on('submit', '#credential-form', submit);
-        $(document).on('click', '.credential-form-reset', reset_form);
-        $(document).on('click', '.remove', remove);
-        $(document).on('click', '.edit', edit);
-
         Storage.get(storage_key, function(result) {
-            $('#url').val(result.url || '');
-            $('#username').val(result.username || '');
-            $('#password').val(result.password || '');
-
+            table.setState(result);
             Storage.set(storage_key, {});
         });
 
         addEventListener('unload', function () {
-            var url = $('#url').val();
-            var username = $('#username').val();
-            var password = $('#password').val();
-
-            var values = {
-                url: url,
-                username: username,
-                password: password
-            };
-            chrome.extension.getBackgroundPage().Storage.set.apply(this, [storage_key, values]);
+            chrome.extension.getBackgroundPage().Storage.set.apply(this, [storage_key, table.getState()]);
         });
     }
 
