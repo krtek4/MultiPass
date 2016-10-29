0bca7994144cecdcf17a500f82491e3bb78bb96b
Gilles Crettenand <gilles.crettenand@liip.ch>
2016-10-29 13:35:01 +0200

    translations everywhere. closes #5

--- js/translator.js
+++ js/translator.js
@@ -1,9 +1,19 @@
 'use strict';
 
+var $ = require('jquery');
+
 module.exports = function() {
     function translate(key) {
         return chrome.i18n.getMessage(key);
     }
+
+    function translateHtml() {
+        $('[data-i18n]').each(function(index, el) {
+            $(el).text(translate($(el).data('i18n')));
+        });
+    }
+
+    translateHtml();
 
     return {
         'translate': translate
