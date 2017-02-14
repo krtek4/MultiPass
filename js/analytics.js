4da986860cf962a7f434b6007e4fb415fc0a9927
Gilles Crettenand <gilles.crettenand@liip.ch>
2017-02-14 14:35:33 +0100

    disable analytics in firefox

--- js/analytics.js
+++ js/analytics.js
@@ -2,14 +2,23 @@
 
 module.exports = function() {
     var manifest = chrome.runtime.getManifest();
+    var browser = manifest.hasOwnProperty('developer') ? 'opera' : manifest.hasOwnProperty('applications') ? 'firefox' : 'chrome';
+
+    if(browser == 'firefox') {
+        var nop = function() {};
+        return {
+            'view': nop,
+            'event': nop,
+            'interaction': nop,
+            'exception': nop
+        };
+    }
 
     /*eslint-disable */
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-    var browser = manifest.hasOwnProperty('developer') ? 'opera' : manifest.hasOwnProperty('applications') ? 'firefox' : 'chrome';
 
     ga('create', 'UA-1168006-9', 'auto');
     ga('set', 'appName', manifest['short_name']);
