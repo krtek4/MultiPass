d35ecb038f81ba10322eb6e988d6444b2cc2e658
Gilles Crettenand <gilles.crettenand@liip.ch>
2017-02-27 11:02:26 +0100

    Prepare the way for disabling analytics. relates #44

--- js/analytics.js
+++ js/analytics.js
@@ -4,47 +4,60 @@ module.exports = function() {
     var manifest = chrome.runtime.getManifest();
     var browser = manifest.hasOwnProperty('developer') ? 'opera' : manifest.hasOwnProperty('applications') ? 'firefox' : 'chrome';
 
-    if(browser == 'firefox') {
-        var nop = function() {};
-        return {
-            'view': nop,
-            'event': nop,
-            'interaction': nop,
-            'exception': nop
+    var queue = [];
+
+    var init = function(enable) {
+        if(browser == 'firefox' || enable == false) {
+            send = function() {};
+            queue = [];
+            return;
+        }
+
+        /*eslint-disable */
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-1168006-9', 'auto');
+        ga('set', 'appName', manifest['short_name']);
+        ga('set', 'appVersion', manifest['version']);
+        ga('set', 'appInstallerId', browser);
+        ga('set', 'checkProtocolTask', function(){}); // Removes failing protocol check. @see: http://stackoverflow.com/a/22152353/1958200
+        // disable displayfeatures as it generates a lot of hits and we are past the limit
+        // ga('require', 'displayfeatures');
+        /*eslint-enable */
+
+        send = function(type, a, b, c, d) {
+            ga('send', type, a, b, c, d);
         };
-    }
 
-    /*eslint-disable */
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+        for(var i in queue) {
+            send.apply(send, queue[i]);
+        }
+    };
 
-    ga('create', 'UA-1168006-9', 'auto');
-    ga('set', 'appName', manifest['short_name']);
-    ga('set', 'appVersion', manifest['version']);
-    ga('set', 'appInstallerId', browser);
-    ga('set', 'checkProtocolTask', function(){}); // Removes failing protocol check. @see: http://stackoverflow.com/a/22152353/1958200
-    // disable displayfeatures as it generates a lot of hits and we are past the limit
-    // ga('require', 'displayfeatures');
+    var send = function(type, a, b, c, d) {
+        queue.push([type, a, b, c, d]);
+    };
 
     var screen = function(name) {
-        ga('send', 'screenview', { 'screenName': name });
+        send('screenview', { 'screenName': name });
     };
 
     var event = function(event, action, value) {
-        ga('send', 'event', event, action, value, { nonInteraction: true });
+        send('event', event, action, value, { nonInteraction: true });
     };
 
     var interaction = function(event, action, value) {
-        ga('send', 'event', event, action, value);
+        send('event', event, action, value);
     };
 
     var exception = function(description) {
-        ga('send', 'exception', { 'exDescription': description, 'exFatal': false });
+        send('exception', { 'exDescription': description, 'exFatal': false });
     };
 
-    /*eslint-enable */
+    init(true);
 
     return {
         'view': screen,
